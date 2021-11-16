ec90bc0436b2a6d6cca4812f04c84772748f0968
munzii <munzii422@gmail.com>
2021-11-16 23:30:48 +0900

    studying

---
+++ src/P15596.java
@@ -0,0 +1,16 @@
+
+public class P15596 {
+	long sum(int[] a) {
+		long ans =0;
+		for(int i=0; i<a.length; i++) {
+			ans +=a[i];
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
---
+++ src/P4673.java
@@ -0,0 +1,15 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class P4673 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		boolean[] n = new boolean[10001];
+		
+
+	}
+
+}
