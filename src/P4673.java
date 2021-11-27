337049610bb66989fa1658c1ad99029c862f1bd1
munzii <munzii422@gmail.com>
2021-11-27 22:31:53 +0900

    studying

--- src/P4673.java
+++ src/P4673.java
@@ -7,9 +7,34 @@ public class P4673 {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		boolean[] n = new boolean[10001];
+		boolean[] x = new boolean[10001];
 		
+		for(int i=1; i<10001; i++) {
+			int n = d(i);
+			
+			if(n<10001) {
+				x[n] = true;
+			}
+		}
+		
+		for(int i=1; i<10001; i++) {
+			if (!x[i]) {
+				bw.write(String.valueOf(i) + "\n");
+			}
+		}
+		bw.flush();
+		bw.close();
 
+	}
+	
+	public static int d(int number) {
+		int sum = number;
+		
+		while(number !=0) {
+			sum = sum + (number%10);
+			number = number/10;
+		}
+		return sum;
 	}
 
 }
